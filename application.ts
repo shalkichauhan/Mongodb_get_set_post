5de1f7ab3028dc7b31e6549b3d2b2ab70a7a8a25
shalkichauhan <shalkichauhan@gmail.com>
2024-05-31 18:08:28 +1000

    added end point

--- application.ts
+++ application.ts
@@ -7,6 +7,12 @@ const pathjoin= path.join(__dirname,'/public')
 app.use(express.static(pathjoin))
 app.use(express.json());
 
+//details
+app.post('/details',(req:any,res:any)=>{
+const myUserdata = req.body.data
+   console.log(" The User Data is :", myUserdata )
+})
+
 
 app.listen(3000,()=>{
     console.log(" server is running!")
